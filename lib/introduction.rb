448ff68afbb468a83c21aa5aec587631668766b8
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-01-24 01:02:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,6 @@ def introduction(name)
   puts "My name is #{name} and I love string interpolation"
 end
 
-def in
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program #{language}."
+end
